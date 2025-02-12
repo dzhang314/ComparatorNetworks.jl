ae26bdd60d87c123d020baec10a48a474d958f86
David K. Zhang <dzhang314@gmail.com>
2025-02-12 01:56:03 -0800

    finish random data and code generators

--- src/ComparatorNetworks.jl
+++ src/ComparatorNetworks.jl
@@ -115,4 +115,82 @@ end
 end
 
 
+# Reduce an integer from the range [0, 63] to the range [0, 52].
+@inline _reduce_63_to_52(i) = ((i << 5) + (i << 4) + (i << 2) + 0x0038) >> 6
+
+
+# Copy bit 0 (LSB) of x to bit positions 0 through n-1.
+@inline function _copy_low_bits(x, n)
+    bit = 0x0000000000000001
+    shifted_bit = (bit << n)
+    low_bit = x & bit
+    mask = shifted_bit - bit
+    value = shifted_bit - low_bit
+    return (x & ~mask) | (value & mask)
+end
+
+
+# Copy bit 51 of x to bit positions 51 through 51-(n-1).
+@inline function _copy_high_bits(x, n)
+    high_bit = (x & 0x0008000000000000) >> 51
+    mask = (0x000FFFFFFFFFFFFF >> n) << n
+    value = 0x0010000000000000 - high_bit
+    return (x & ~mask) | (value & mask)
+end
+
+
+# Generate a random vector of 64-bit floating-point numbers for testing
+# floating-point accumulation networks (FPANs). The generated values have
+# uniformly distributed signs and exponents in the range [-511, +512], and
+# their mantissas are biased to have many leading/trailing zeros/ones.
+function _rand_vec_f64()
+    sign_exponent_data = _rand_vec_16()
+    sign_bits = (sign_exponent_data << 48) & 0x8000000000000000
+    exponents = ((sign_exponent_data & 0x03FF) + 0x0200) << 52
+    mantissa_data = _rand_vec_64()
+    i = mantissa_data >> 58
+    j = (mantissa_data >> 52) & 0x3F
+    low_index = _reduce_63_to_52(min(i, j))
+    high_index = _reduce_63_to_52(max(i, j))
+    mantissas = mantissa_data & 0x000FFFFFFFFFFFFF
+    mantissas = _copy_low_bits(mantissas, low_index)
+    mantissas = _copy_high_bits(mantissas, high_index)
+    return reinterpret(Vec{8,Float64}, sign_bits | exponents | mantissas)
+end
+
+
+################################################################################
+
+
+@inline _bitwise_minmax(x, y) = (x & y, x | y)
+
+
+@inline function _two_sum(x, y)
+    s = x + y
+    x_prime = s - y
+    y_prime = s - x_prime
+    delta_x = x - x_prime
+    delta_y = y - y_prime
+    e = delta_x + delta_y
+    return (s, e)
+end
+
+
+function _generate_code(network::ComparatorNetwork{N}, comparator) where {N}
+    xs = [Symbol('x', i) for i in Base.OneTo(N)]
+    body = Expr[]
+    push!(body, Expr(:meta, :inline))
+    push!(body, Expr(:(=), Expr(:tuple, xs...), :x))
+    for (i, j) in network.comparators
+        @assert i < j
+        push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[j]),
+            Expr(:call, comparator, xs[i], xs[j])))
+    end
+    push!(body, Expr(:return, Expr(:tuple, xs...)))
+    return Expr(:->, :x, Expr(:block, body...))
+end
+
+
+################################################################################
+
 end # module ComparatorNetworks
