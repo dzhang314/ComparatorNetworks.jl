74fec3a90d34e0adc3f2bffe70de77fd73379df8
David K. Zhang <dzhang314@gmail.com>
2025-02-16 14:25:11 -0800

    further reorganize code

--- src/ComparatorNetworks.jl
+++ src/ComparatorNetworks.jl
@@ -272,6 +272,209 @@ function canonize(network::ComparatorNetwork{N}) where {N}
 end
 
 
+########################################################### COMPARATOR FUNCTIONS
+
+
+export bitminmax, two_sum
+
+
+@inline bitminmax(x, y) = (x & y, x | y)
+
+
+@inline function two_sum(x, y)
+    s = x + y
+    x_prime = s - y
+    y_prime = s - x_prime
+    delta_x = x - x_prime
+    delta_y = y - y_prime
+    e = delta_x + delta_y
+    return (s, e)
+end
+
+
+################################################### COMPARATOR NETWORK EXECUTION
+
+
+@inline function _unsafe_run_comparator_network!(
+    data::AbstractVector{T},
+    network::ComparatorNetwork{N},
+    comparator::F,
+) where {T,N,F}
+    for (i, j) in network.comparators
+        @inbounds @inline data[i], data[j] =
+            comparator(data[i], data[j])
+    end
+    return data
+end
+
+
+############################################# COMPARATOR NETWORK CODE GENERATION
+
+
+export generate_code
+
+
+function generate_code(network::ComparatorNetwork{N}, comparator) where {N}
+    xs = [Symbol('x', i) for i in Base.OneTo(N)]
+    body = Expr[]
+    push!(body, Expr(:meta, :inline))
+    push!(body, Expr(:(=), Expr(:tuple, xs...), :x))
+    for (i, j) in network.comparators
+        @assert i < j
+        push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[j]),
+            Expr(:call, comparator, xs[i], xs[j])))
+    end
+    push!(body, Expr(:return, Expr(:tuple, xs...)))
+    return Expr(:->, :x, Expr(:block, body...))
+end
+
+
+############################################################ CRITERION FUNCTIONS
+
+
+export bitsorted
+
+
+@inline function bitsorted(data::AbstractVector{T}) where {T}
+    result = ~zero(T)
+    @simd for i = firstindex(data):lastindex(data)-1
+        @inbounds result &= data[i+1] | ~data[i]
+    end
+    return all(iszero(~result))
+end
+
+
+############################################################## COMPARATOR PASSES
+
+
+function _inline_pass_expr(comparator::Symbol, method::Symbol, N::Int)
+    xs = [Symbol('x', i) for i in Base.OneTo(N)]
+    body = Expr[]
+    push!(body, Expr(:meta, :inline))
+    push!(body, Expr(:(=), Expr(:tuple, xs...), :x))
+    if method == :top_down
+        for i in 1:N-1
+            push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
+                Expr(:call, comparator, xs[i], xs[i+1])))
+        end
+    elseif method == :bottom_up
+        for i = N-1:-1:1
+            push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
+                Expr(:call, comparator, xs[i], xs[i+1])))
+        end
+    elseif method == :riffle
+        for i = 1:2:N-1
+            push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
+                Expr(:call, comparator, xs[i], xs[i+1])))
+        end
+        for i = 2:2:N-1
+            push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
+                Expr(:call, comparator, xs[i], xs[i+1])))
+        end
+    else
+        @assert false
+    end
+    push!(body, Expr(:return, Expr(:tuple, xs...)))
+    return Expr(:block, body...)
+end
+
+
+@generated function _top_down_bitbubble(x::NTuple{N,T}) where {N,T}
+    return _inline_pass_expr(:bitminmax, :top_down, N)
+end
+
+
+@generated function _top_down_accumulate(x::NTuple{N,T}) where {N,T}
+    return _inline_pass_expr(:two_sum, :top_down, N)
+end
+
+
+@generated function _bottom_up_bitbubble(x::NTuple{N,T}) where {N,T}
+    return _inline_pass_expr(:bitminmax, :bottom_up, N)
+end
+
+
+@generated function _bottom_up_accumulate(x::NTuple{N,T}) where {N,T}
+    return _inline_pass_expr(:two_sum, :bottom_up, N)
+end
+
+
+@generated function _riffle_bitbubble(x::NTuple{N,T}) where {N,T}
+    return _inline_pass_expr(:bitminmax, :riffle, N)
+end
+
+
+@generated function _riffle_accumulate(x::NTuple{N,T}) where {N,T}
+    return _inline_pass_expr(:two_sum, :riffle, N)
+end
+
+
+@inline function _top_down_bitsort(x::NTuple{N,T}) where {N,T}
+    while true
+        x_next = _top_down_bitbubble(x)
+        if x_next === x
+            return x
+        end
+        x = x_next
+    end
+end
+
+
+@inline function _top_down_normalize(x::NTuple{N,T}) where {N,T}
+    while true
+        x_next = _top_down_accumulate(x)
+        if x_next === x
+            return x
+        end
+        x = x_next
+    end
+end
+
+
+@inline function _bottom_up_bitsort(x::NTuple{N,T}) where {N,T}
+    while true
+        x_next = _bottom_up_bitbubble(x)
+        if x_next === x
+            return x
+        end
+        x = x_next
+    end
+end
+
+
+@inline function _bottom_up_normalize(x::NTuple{N,T}) where {N,T}
+    while true
+        x_next = _bottom_up_accumulate(x)
+        if x_next === x
+            return x
+        end
+        x = x_next
+    end
+end
+
+
+@inline function _riffle_bitsort(x::NTuple{N,T}) where {N,T}
+    while true
+        x_next = _riffle_bitbubble(x)
+        if x_next === x
+            return x
+        end
+        x = x_next
+    end
+end
+
+
+@inline function _riffle_normalize(x::NTuple{N,T}) where {N,T}
+    while true
+        x_next = _riffle_accumulate(x)
+        if x_next === x
+            return x
+        end
+        x = x_next
+    end
+end
+
+
 ############################################################### BINARY TEST DATA
 
 
@@ -688,128 +891,6 @@ const SORTED_BIT_STRINGS_10 = (
 @inline SORTED_BIT_STRINGS(::Val{10}) = SORTED_BIT_STRINGS_10
 
 
-########################################################### COMPARATOR FUNCTIONS
-
-
-export bitminmax, two_sum
-
-
-@inline bitminmax(x, y) = (x & y, x | y)
-
-
-@inline function two_sum(x, y)
-    s = x + y
-    x_prime = s - y
-    y_prime = s - x_prime
-    delta_x = x - x_prime
-    delta_y = y - y_prime
-    e = delta_x + delta_y
-    return (s, e)
-end
-
-
-############################################################ CRITERION FUNCTIONS
-
-
-export bitsorted
-
-
-@inline function bitsorted(data::AbstractVector{T}) where {T}
-    result = ~zero(T)
-    @simd for i = firstindex(data):lastindex(data)-1
-        @inbounds result &= data[i+1] | ~data[i]
-    end
-    return all(iszero(~result))
-end
-
-
-############################################################## COMPARATOR PASSES
-
-
-function _inline_pass_expr(comparator::Symbol, method::Symbol, N::Int)
-    xs = [Symbol('x', i) for i in Base.OneTo(N)]
-    body = Expr[]
-    push!(body, Expr(:meta, :inline))
-    push!(body, Expr(:(=), Expr(:tuple, xs...), :x))
-    if method == :top_down
-        for i in 1:N-1
-            push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
-                Expr(:call, comparator, xs[i], xs[i+1])))
-        end
-    elseif method == :bottom_up
-        for i = N-1:-1:1
-            push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
-                Expr(:call, comparator, xs[i], xs[i+1])))
-        end
-    elseif method == :riffle
-        for i = 1:2:N-1
-            push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
-                Expr(:call, comparator, xs[i], xs[i+1])))
-        end
-        for i = 2:2:N-1
-            push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[i+1]),
-                Expr(:call, comparator, xs[i], xs[i+1])))
-        end
-    else
-        @assert false
-    end
-    push!(body, Expr(:return, Expr(:tuple, xs...)))
-    return Expr(:block, body...)
-end
-
-
-@generated function _top_down_bitbubble(x::NTuple{N,T}) where {N,T}
-    return _inline_pass_expr(:bitminmax, :top_down, N)
-end
-
-
-@generated function _top_down_accumulate(x::NTuple{N,T}) where {N,T}
-    return _inline_pass_expr(:two_sum, :top_down, N)
-end
-
-
-@generated function _bottom_up_bitbubble(x::NTuple{N,T}) where {N,T}
-    return _inline_pass_expr(:bitminmax, :bottom_up, N)
-end
-
-
-@generated function _bottom_up_accumulate(x::NTuple{N,T}) where {N,T}
-    return _inline_pass_expr(:two_sum, :bottom_up, N)
-end
-
-
-@generated function _riffle_bitbubble(x::NTuple{N,T}) where {N,T}
-    return _inline_pass_expr(:bitminmax, :riffle, N)
-end
-
-
-@generated function _riffle_accumulate(x::NTuple{N,T}) where {N,T}
-    return _inline_pass_expr(:two_sum, :riffle, N)
-end
-
-
-@inline function _top_down_bitsort(x::NTuple{N,T}) where {N,T}
-    while true
-        x_next = _top_down_bitbubble(x)
-        if x_next === x
-            return x
-        end
-        x = x_next
-    end
-end
-
-
-@inline function _top_down_normalize(x::NTuple{N,T}) where {N,T}
-    while true
-        x_next = _top_down_accumulate(x)
-        if x_next === x
-            return x
-        end
-        x = x_next
-    end
-end
-
-
 @assert _top_down_bitsort(ALL_BIT_STRINGS_3) === SORTED_BIT_STRINGS_3
 @assert _top_down_bitsort(ALL_BIT_STRINGS_4) === SORTED_BIT_STRINGS_4
 @assert _top_down_bitsort(ALL_BIT_STRINGS_5) === SORTED_BIT_STRINGS_5
@@ -820,28 +901,6 @@ end
 @assert _top_down_bitsort(ALL_BIT_STRINGS_10) === SORTED_BIT_STRINGS_10
 
 
-@inline function _bottom_up_bitsort(x::NTuple{N,T}) where {N,T}
-    while true
-        x_next = _bottom_up_bitbubble(x)
-        if x_next === x
-            return x
-        end
-        x = x_next
-    end
-end
-
-
-@inline function _bottom_up_normalize(x::NTuple{N,T}) where {N,T}
-    while true
-        x_next = _bottom_up_accumulate(x)
-        if x_next === x
-            return x
-        end
-        x = x_next
-    end
-end
-
-
 @assert _bottom_up_bitsort(ALL_BIT_STRINGS_3) === SORTED_BIT_STRINGS_3
 @assert _bottom_up_bitsort(ALL_BIT_STRINGS_4) === SORTED_BIT_STRINGS_4
 @assert _bottom_up_bitsort(ALL_BIT_STRINGS_5) === SORTED_BIT_STRINGS_5
@@ -850,28 +909,6 @@ end
 @assert _bottom_up_bitsort(ALL_BIT_STRINGS_8) === SORTED_BIT_STRINGS_8
 @assert _bottom_up_bitsort(ALL_BIT_STRINGS_9) === SORTED_BIT_STRINGS_9
 @assert _bottom_up_bitsort(ALL_BIT_STRINGS_10) === SORTED_BIT_STRINGS_10
-
-
-@inline function _riffle_bitsort(x::NTuple{N,T}) where {N,T}
-    while true
-        x_next = _riffle_bitbubble(x)
-        if x_next === x
-            return x
-        end
-        x = x_next
-    end
-end
-
-
-@inline function _riffle_normalize(x::NTuple{N,T}) where {N,T}
-    while true
-        x_next = _riffle_accumulate(x)
-        if x_next === x
-            return x
-        end
-        x = x_next
-    end
-end
 
 
 @assert _riffle_bitsort(ALL_BIT_STRINGS_3) === SORTED_BIT_STRINGS_3
@@ -965,27 +1002,6 @@ const BITS_PER_BYTE = 8
     item_index, bit_index = divrem(n, bit_size)
     item_index += 1
     return Base.setindex(x, _flip_bit(x[item_index], bit_index), item_index)
-end
-
-
-################################################################ CODE GENERATION
-
-
-export generate_code
-
-
-function generate_code(network::ComparatorNetwork{N}, comparator) where {N}
-    xs = [Symbol('x', i) for i in Base.OneTo(N)]
-    body = Expr[]
-    push!(body, Expr(:meta, :inline))
-    push!(body, Expr(:(=), Expr(:tuple, xs...), :x))
-    for (i, j) in network.comparators
-        @assert i < j
-        push!(body, Expr(:(=), Expr(:tuple, xs[i], xs[j]),
-            Expr(:call, comparator, xs[i], xs[j])))
-    end
-    push!(body, Expr(:return, Expr(:tuple, xs...)))
-    return Expr(:->, :x, Expr(:block, body...))
 end
 
 
