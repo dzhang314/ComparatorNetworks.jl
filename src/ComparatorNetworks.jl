a4434a2bf17136e939a091174b3b492698554bc5
David K. Zhang <dzhang314@gmail.com>
2025-02-16 14:14:55 -0800

    reorganize code and add section labels

--- src/ComparatorNetworks.jl
+++ src/ComparatorNetworks.jl
@@ -2,7 +2,7 @@ module ComparatorNetworks
 
 using SIMD: Vec
 
-################################################################################
+############################################## COMPARATOR NETWORK DATA STRUCTURE
 
 
 export ComparatorNetwork
@@ -30,28 +30,28 @@ end
 
 
 @inline Base.length(network::ComparatorNetwork{N}) where {N} =
-    length(network.comparators)
+    @inline length(network.comparators)
 @inline Base.:(==)(a::ComparatorNetwork{N}, b::ComparatorNetwork{N}) where {N} =
-    (a.comparators == b.comparators)
+    @inline (a.comparators == b.comparators)
 @inline Base.copy(network::ComparatorNetwork{N}) where {N} =
-    ComparatorNetwork{N}(network.comparators)
+    @inline ComparatorNetwork{N}(network.comparators)
 @inline Base.hash(network::ComparatorNetwork{N}, h::UInt) where {N} =
-    hash(network.comparators, hash(N, h))
+    @inline hash(network.comparators, hash(N, h))
 @inline Base.isless(
     a::ComparatorNetwork{M},
     b::ComparatorNetwork{N},
-) where {M,N} = isless(
+) where {M,N} = @inline isless(
     (M, length(a.comparators), a.comparators),
     (N, length(b.comparators), b.comparators))
 
 
-################################################################################
+############################################################## DEPTH COMPUTATION
 
 
 export depth
 
 
-function depth(network::ComparatorNetwork{N}) where {N}
+@inline function depth(network::ComparatorNetwork{N}) where {N}
     generation = ntuple(_ -> 0, Val{N}())
     @inbounds for (i, j) in network.comparators
         age = max(generation[i], generation[j]) + 1
@@ -62,7 +62,7 @@ function depth(network::ComparatorNetwork{N}) where {N}
 end
 
 
-################################################################################
+################################################################ CANONICAL FORMS
 
 
 export canonize
@@ -272,7 +272,7 @@ function canonize(network::ComparatorNetwork{N}) where {N}
 end
 
 
-################################################################################
+############################################################### BINARY TEST DATA
 
 
 export ALL_BIT_STRINGS, SORTED_BIT_STRINGS
@@ -688,65 +688,7 @@ const SORTED_BIT_STRINGS_10 = (
 @inline SORTED_BIT_STRINGS(::Val{10}) = SORTED_BIT_STRINGS_10
 
 
-################################################################################
-
-
-@inline _rand_vec_64(::Val{N}) where {N} =
-    Vec{N,UInt64}(ntuple(_ -> rand(UInt64), Val{N}()))
-
-
-@inline function _rand_vec_16(::Val{N}) where {N}
-    data = ntuple(_ -> rand(UInt64), Val{(N + 3) >> 2}())
-    return Vec{N,UInt64}(ntuple(
-        i -> (data[(i+3)>>2] >> ((i & 3) << 4)) & 0xFFFF, Val{N}()))
-end
-
-
-# Reduce an integer from the range [0, 63] to the range [0, 52].
-@inline _reduce_63_to_52(i) = ((i << 5) + (i << 4) + (i << 2) + 0x0038) >> 6
-
-
-# Copy bit 0 (LSB) of x to bit positions 0 through n-1.
-@inline function _copy_low_bits(x, n)
-    bit = 0x0000000000000001
-    shifted_bit = (bit << n)
-    low_bit = x & bit
-    mask = shifted_bit - bit
-    value = shifted_bit - low_bit
-    return (x & ~mask) | (value & mask)
-end
-
-
-# Copy bit 51 of x to bit positions 51 through 51-(n-1).
-@inline function _copy_high_bits(x, n)
-    high_bit = (x & 0x0008000000000000) >> 51
-    mask = (0x000FFFFFFFFFFFFF >> n) << n
-    value = 0x0010000000000000 - high_bit
-    return (x & ~mask) | (value & mask)
-end
-
-
-# Generate a random vector of 64-bit floating-point numbers for testing
-# floating-point accumulation networks (FPANs). The generated values have
-# uniformly distributed signs and exponents in the range [-511, +512], and
-# their mantissas are biased to have many leading/trailing zeros/ones.
-@inline function _rand_vec_f64(::Val{N}) where {N}
-    sign_exponent_data = _rand_vec_16(Val{N}())
-    sign_bits = (sign_exponent_data << 48) & 0x8000000000000000
-    exponents = ((sign_exponent_data & 0x03FF) + 0x0200) << 52
-    mantissa_data = _rand_vec_64(Val{N}())
-    i = mantissa_data >> 58
-    j = (mantissa_data >> 52) & 0x3F
-    low_index = _reduce_63_to_52(min(i, j))
-    high_index = _reduce_63_to_52(max(i, j))
-    mantissas = mantissa_data & 0x000FFFFFFFFFFFFF
-    mantissas = _copy_low_bits(mantissas, low_index)
-    mantissas = _copy_high_bits(mantissas, high_index)
-    return reinterpret(Vec{N,Float64}, sign_bits | exponents | mantissas)
-end
-
-
-################################################################################
+########################################################### COMPARATOR FUNCTIONS
 
 
 export bitminmax, two_sum
@@ -764,6 +706,9 @@ export bitminmax, two_sum
     e = delta_x + delta_y
     return (s, e)
 end
+
+
+############################################################## COMPARATOR PASSES
 
 
 function _inline_pass_expr(comparator::Symbol, method::Symbol, N::Int)
@@ -924,11 +869,69 @@ end
 @assert _riffle_bitsort(ALL_BIT_STRINGS_10) === SORTED_BIT_STRINGS_10
 
 
+#################################################### RANDOM TEST CASE GENERATION
+
+
+@inline _rand_vec_64(::Val{N}) where {N} =
+    Vec{N,UInt64}(ntuple(_ -> rand(UInt64), Val{N}()))
+
+
+@inline function _rand_vec_16(::Val{N}) where {N}
+    data = ntuple(_ -> rand(UInt64), Val{(N + 3) >> 2}())
+    return Vec{N,UInt64}(ntuple(
+        i -> (data[(i+3)>>2] >> ((i & 3) << 4)) & 0xFFFF, Val{N}()))
+end
+
+
+# Reduce an integer from the range [0, 63] to the range [0, 52].
+@inline _reduce_63_to_52(i) = ((i << 5) + (i << 4) + (i << 2) + 0x0038) >> 6
+
+
+# Copy bit 0 (LSB) of x to bit positions 0 through n-1.
+@inline function _copy_low_bits(x, n)
+    bit = 0x0000000000000001
+    shifted_bit = (bit << n)
+    low_bit = x & bit
+    mask = shifted_bit - bit
+    value = shifted_bit - low_bit
+    return (x & ~mask) | (value & mask)
+end
+
+
+# Copy bit 51 of x to bit positions 51 through 51-(n-1).
+@inline function _copy_high_bits(x, n)
+    high_bit = (x & 0x0008000000000000) >> 51
+    mask = (0x000FFFFFFFFFFFFF >> n) << n
+    value = 0x0010000000000000 - high_bit
+    return (x & ~mask) | (value & mask)
+end
+
+
+# Generate a random vector of 64-bit floating-point numbers for testing
+# floating-point accumulation networks (FPANs). The generated values have
+# uniformly distributed signs and exponents in the range [-511, +512], and
+# their mantissas are biased to have many leading/trailing zeros/ones.
+@inline function _rand_vec_f64(::Val{N}) where {N}
+    sign_exponent_data = _rand_vec_16(Val{N}())
+    sign_bits = (sign_exponent_data << 48) & 0x8000000000000000
+    exponents = ((sign_exponent_data & 0x03FF) + 0x0200) << 52
+    mantissa_data = _rand_vec_64(Val{N}())
+    i = mantissa_data >> 58
+    j = (mantissa_data >> 52) & 0x3F
+    low_index = _reduce_63_to_52(min(i, j))
+    high_index = _reduce_63_to_52(max(i, j))
+    mantissas = mantissa_data & 0x000FFFFFFFFFFFFF
+    mantissas = _copy_low_bits(mantissas, low_index)
+    mantissas = _copy_high_bits(mantissas, high_index)
+    return reinterpret(Vec{N,Float64}, sign_bits | exponents | mantissas)
+end
+
+
 @inline _rand_vec_mf64(::Val{M}, ::Val{N}) where {M,N} =
     _riffle_normalize(ntuple(_ -> _rand_vec_f64(Val{M}()), Val{N}()))
 
 
-################################################################################
+######################################################### TEST CASE OPTIMIZATION
 
 
 @inline function _flip_bit(x::T, n::Int) where {T}
@@ -950,7 +953,7 @@ const BITS_PER_BYTE = 8
 end
 
 
-################################################################################
+################################################################ CODE GENERATION
 
 
 export generate_code
