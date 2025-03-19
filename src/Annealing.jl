b705ff033173d3f016be879c7809c2b89737d6f3
David K. Zhang <dzhang314@gmail.com>
2025-03-18 18:59:56 -0700

    add annealing optimizer

---
+++ src/Annealing.jl
@@ -0,0 +1,183 @@
+module Annealing
+
+using ..ComparatorNetworks: ComparatorNetwork, _unsafe_test_comparator_network!,
+    _random_comparator, _unsafe_generate_comparator_network!
+
+################################################################################
+
+
+export pareto_dominates, pareto_push!, optimize_comparator_network
+
+
+function _unsafe_mutate_comparator_network!(
+    temp::AbstractVector{T},
+    network::ComparatorNetwork{N},
+    comparator::C,
+    test_cases::AbstractVector{NTuple{N,T}},
+    property::P,
+    insert_weight::Int,
+    delete_weight::Int,
+    swap_weight::Int,
+    duration_ns::UInt64,
+) where {N,T,C,P}
+    start_ns = time_ns()
+    num_comparators = length(network.comparators)
+    w1 = insert_weight
+    w2 = w1 + delete_weight
+    w3 = w2 + swap_weight
+    while true
+        w = rand(1:w3)
+        if w <= w1 # insert
+            i = rand(1:num_comparators+1)
+            insert!(network.comparators, i, _random_comparator(Val{N}()))
+            pass = _unsafe_test_comparator_network!(
+                temp, network, comparator, test_cases, property)
+            if pass
+                return network
+            end
+            deleteat!(network.comparators, i)
+        elseif (w <= w2) & (num_comparators >= 1) # delete
+            i = rand(1:num_comparators)
+            @inbounds original_comparator = network.comparators[i]
+            deleteat!(network.comparators, i)
+            pass = _unsafe_test_comparator_network!(
+                temp, network, comparator, test_cases, property)
+            if pass
+                return network
+            end
+            insert!(network.comparators, i, original_comparator)
+        elseif (w <= w3) & (num_comparators >= 2) # swap
+            i = rand(1:num_comparators)
+            j = rand(1:num_comparators-1)
+            j += (j >= i)
+            @inbounds network.comparators[i], network.comparators[j] =
+                network.comparators[j], network.comparators[i]
+            pass = _unsafe_test_comparator_network!(
+                temp, network, comparator, test_cases, property)
+            if pass
+                return network
+            end
+            @inbounds network.comparators[i], network.comparators[j] =
+                network.comparators[j], network.comparators[i]
+        end
+        if time_ns() - start_ns >= duration_ns
+            return network
+        end
+    end
+end
+
+
+@inline function pareto_dominates(s, t)
+    all_le = true
+    any_lt = false
+    @inbounds for i in eachindex(s, t)
+        all_le &= (s[i] <= t[i])
+        any_lt |= (s[i] < t[i])
+    end
+    return all_le & any_lt
+end
+
+
+function pareto_push!(dict::Dict{K,Set{V}}, key::K, value::V) where {K,V}
+    if haskey(dict, key)
+        push!(dict[key], value)
+    else
+        worse_keys = K[]
+        for other_key in keys(dict)
+            if pareto_dominates(other_key, key)
+                return dict
+            elseif pareto_dominates(key, other_key)
+                push!(worse_keys, other_key)
+            end
+        end
+        for worse_key in worse_keys
+            delete!(dict, worse_key)
+        end
+        dict[key] = Set((value,))
+    end
+    return dict
+end
+
+
+function _unsafe_anneal_comparator_network!(
+    temp_data::AbstractVector{T},
+    temp_network::ComparatorNetwork{N},
+    comparator::C,
+    test_cases::AbstractVector{NTuple{N,T}},
+    property::P,
+    scoring_function::S,
+    insert_rate_function::F1,
+    delete_rate_function::F2,
+    swap_rate_function::F3,
+    start_ns::UInt64,
+    duration_ns::UInt64,
+) where {N,T,C,P,S,F1,F2,F3}
+    initial_score = scoring_function(temp_network)
+    result = Dict{typeof(initial_score),Set{ComparatorNetwork{N}}}()
+    pareto_push!(result, initial_score, copy(temp_network))
+    t = 1
+    while true
+        insert_weight = max(1, ceil(Int, insert_rate_function(t)))
+        delete_weight = max(1, ceil(Int, delete_rate_function(t)))
+        swap_weight = max(1, ceil(Int, swap_rate_function(t)))
+        elapsed_ns = time_ns() - start_ns
+        if elapsed_ns >= duration_ns
+            return result
+        end
+        remaining_ns = duration_ns - elapsed_ns
+        _unsafe_mutate_comparator_network!(
+            temp_data, temp_network, comparator, test_cases, property,
+            insert_weight, delete_weight, swap_weight, remaining_ns)
+        pareto_push!(result, scoring_function(temp_network), copy(temp_network))
+        t += 1
+    end
+end
+
+
+function optimize_comparator_network(
+    network::ComparatorNetwork{N},
+    comparator::C,
+    test_cases::AbstractVector{NTuple{N,T}},
+    property::P,
+    scoring_function::S,
+    insert_rate_function::F1,
+    delete_rate_function::F2,
+    swap_rate_function::F3,
+    duration_ns::UInt64,
+) where {N,T,C,P,S,F1,F2,F3}
+    start_ns = time_ns()
+    temp_data = Vector{T}(undef, N)
+    temp_network = copy(network)
+    return _unsafe_anneal_comparator_network!(
+        temp_data, temp_network,
+        comparator, test_cases, property, scoring_function,
+        insert_rate_function, delete_rate_function, swap_rate_function,
+        start_ns, duration_ns)
+end
+
+
+function optimize_comparator_network(
+    comparator::C,
+    test_cases::AbstractVector{NTuple{N,T}},
+    property::P,
+    scoring_function::S,
+    insert_rate_function::F1,
+    delete_rate_function::F2,
+    swap_rate_function::F3,
+    duration_ns::UInt64,
+) where {N,T,C,P,S,F1,F2,F3}
+    start_ns = time_ns()
+    temp_data = Vector{T}(undef, N)
+    temp_network = _unsafe_generate_comparator_network!(
+        temp_data, comparator, test_cases, property)
+    return _unsafe_anneal_comparator_network!(
+        temp_data, temp_network,
+        comparator, test_cases, property, scoring_function,
+        insert_rate_function, delete_rate_function, swap_rate_function,
+        start_ns, duration_ns)
+end
+
+
+################################################################################
+
+end # module Annealing
--- src/ComparatorNetworks.jl
+++ src/ComparatorNetworks.jl
@@ -70,6 +70,12 @@ function Base.isless(
 end
 
 
+include("Output.jl")
+using .Output
+export hexfloat
+export svg_string
+
+
 ############################################################## DEPTH COMPUTATION
 
 
@@ -331,8 +337,7 @@ end
 ################################################## COMPARATOR NETWORK GENERATION
 
 
-export test_comparator_network, prune!, generate_comparator_network,
-    mutate_comparator_network!
+export test_comparator_network, prune!, generate_comparator_network
 
 
 @inline function _unsafe_test_comparator_network!(
@@ -440,76 +445,9 @@ generate_comparator_network(
     Vector{T}(undef, N), comparator, test_cases, property)
 
 
-function _unsafe_mutate_comparator_network!(
-    temp::AbstractVector{T},
-    network::ComparatorNetwork{N},
-    comparator::C,
-    test_cases::AbstractVector{NTuple{N,T}},
-    property::P,
-    insert_weight::Int,
-    delete_weight::Int,
-    swap_weight::Int,
-    duration_ns::UInt64,
-) where {N,T,C,P}
-    num_comparators = length(network.comparators)
-    w1 = insert_weight
-    w2 = w1 + delete_weight
-    w3 = w2 + swap_weight
-    start_ns = time_ns()
-    while true
-        w = rand(1:w3)
-        if w <= w1 # insert
-            i = rand(1:num_comparators+1)
-            insert!(network.comparators, i, _random_comparator(Val{N}()))
-            pass = _unsafe_test_comparator_network!(
-                temp, network, comparator, test_cases, property)
-            if pass
-                return network
-            end
-            deleteat!(network.comparators, i)
-        elseif (w <= w2) & (num_comparators >= 1) # delete
-            i = rand(1:num_comparators)
-            @inbounds original_comparator = network.comparators[i]
-            deleteat!(network.comparators, i)
-            pass = _unsafe_test_comparator_network!(
-                temp, network, comparator, test_cases, property)
-            if pass
-                return network
-            end
-            insert!(network.comparators, i, original_comparator)
-        elseif (w <= w3) & (num_comparators >= 2) # swap
-            i = rand(1:num_comparators)
-            j = rand(1:num_comparators-1)
-            j += (j >= i)
-            @inbounds network.comparators[i], network.comparators[j] =
-                network.comparators[j], network.comparators[i]
-            pass = _unsafe_test_comparator_network!(
-                temp, network, comparator, test_cases, property)
-            if pass
-                return network
-            end
-            @inbounds network.comparators[i], network.comparators[j] =
-                network.comparators[j], network.comparators[i]
-        end
-        if time_ns() - start_ns >= duration_ns
-            return network
-        end
-    end
-end
-
-
-mutate_comparator_network!(
-    network::ComparatorNetwork{N},
-    comparator::C,
-    test_cases::AbstractVector{NTuple{N,T}},
-    property::P,
-    insert_weight::Int,
-    delete_weight::Int,
-    swap_weight::Int,
-    duration_ns::UInt64,
-) where {N,T,C,P} = _unsafe_mutate_comparator_network!(
-    Vector{T}(undef, N), network, comparator, test_cases, property,
-    insert_weight, delete_weight, swap_weight, duration_ns)
+include("Annealing.jl")
+using .Annealing
+export pareto_dominates, pareto_push!, optimize_comparator_network
 
 
 ######################################################### TEST CASE MANIPULATION
@@ -692,9 +630,9 @@ function find_worst_case_mfadd_inputs(
     ::Val{M},
     duration_ns::UInt64,
 ) where {N,X,Y,Z,M}
+    start_ns = time_ns()
     @assert X + Y == N
     @assert 0 < Z <= N
-    start_ns = time_ns()
     max_error = zero(Float64)
     worst_case_x = ntuple(_ -> zero(Float64), Val{X}())
     worst_case_y = ntuple(_ -> zero(Float64), Val{Y}())
@@ -798,10 +736,5 @@ end
 
 
 ################################################################################
-
-include("Output.jl")
-using .Output
-export hexfloat
-export svg_string
 
 end # module ComparatorNetworks
